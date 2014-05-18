b98ad4d3eed12d0eab6f183e05493c1e8f8ee608
dmitry <d.demidov@itransition.com>
2014-05-18 12:33:29 +0300

    Test database is added.

---
+++ logs/README.txt
@@ -0,0 +1,1 @@
+This is just a file to add 'empty' folder into git repository.
---
+++ src/core/test_settings.py
@@ -0,0 +1,8 @@
+from core.settings import *
+
+DATABASES = {
+    'default': {
+        'NAME': 'test_db.sql',
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
+}
